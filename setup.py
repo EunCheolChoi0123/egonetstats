b0d4d3f88acbe6f637346cd9c49d276d6af9d709
EunCheolChoi0123 <124309747+EunCheolChoi0123@users.noreply.github.com>
2025-04-04 14:10:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='egonetstats',
-    version='0.1.0',
+    version='0.1.1',
     description='A small package for computing egonet composition statistics',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Eun Cheol Choi',
+    author_email='euncheol@usc.edu',
     packages=find_packages(),
     install_requires=[
         'pandas',
